ffd1f2181162857d6756f0669226d641369f58ca
Kodirov2samir <kodirovsamir174@gmail.com>
2025-10-04 21:23:50 +0500

    Omega

---
+++ Terms/BigOmega/bigOmega.js
@@ -0,0 +1,6 @@
+//the symbol Ω(omega) is used to measure the best possible outcome
+//Both linear and binary have Omega(1)
+//but others for example O(n log n) =>Omega(n)
+//so basically in omega there are only two cases
+//Omega(1)
+//Omega(2)
